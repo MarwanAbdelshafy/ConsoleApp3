76b27021ac20ce74074631226ab9a051ea3d2b6c
MarwanAbdelshafy <marawan00500@gmail.com>
2024-12-11 21:30:40 +0200

    Write Demo

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Demo
+
+
+            #region Error types
+
+            //Syntax Error  == wrong word 
+            //Logical Error == wrong logic 
+            //Run Time Error == like /0
+            //Warning == not error but warning to posiple wrong
+            #endregion
+
+
+            #region DeclareVariables
+            /*
+            int number;
+            number = 1000;
+            Console.WriteLine(number);
+            */
+
+            #endregion
+
+
+            #region DataType
+
+            //Struct (Byte -- Short -- int -- Long //Float -- Double -- Decimal -- Char -- Bool -- Datetime)
+            //Enum
+            //Class (Object -- String -- Array -- Delegate)
+            //Interface
+            #endregion
+
+
+            #region Value Types
+            /*
+            int x;
+            x = 20;
+            int y;
+            y = 10;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+
+            y = x;
+            x++;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            */
+
+            #endregion
+
+
+
+            #endregion
         }
     }
 }
